6479d7936d22f88ad5acc02d6d60b206776f2e15
PR <premr3997@gmail.com>
2023-03-19 20:53:24 +0530

    Added new function to implement visitors counter logic

--- index.js
+++ index.js
@@ -2,3 +2,20 @@
     var content = document.getElementById(id);
     content.classList.toggle("reveal-content");
 }
+
+window.onload = function() {
+
+    var visitorsCount = 100;
+    var countElement = document.getElementById('visitors');
+    var initialCount = 0;
+
+
+    var counter = setInterval(function() {
+        if(initialCount < visitorsCount) {
+            initialCount +=1;
+            countElement.innerHTML = initialCount;
+        } else {
+            clearInterval(counter);
+        }
+    }, 10);
+};
