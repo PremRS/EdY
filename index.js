c8a406590783868b2f82e31d952c507c84e12805
PR <premr3997@gmail.com>
2023-04-13 23:08:09 +0530

    Added the logic to render content based on given data.

--- index.js
+++ index.js
@@ -1,18 +1,88 @@
- function expandContent(element, id) {
+const CONTENTS = [{
+    src: 'cheatsheet',
+    id: 'content-cheatsheet',
+    title: 'Cheatsheet'
+}, {
+    src: 'finance',
+    id: 'content-finance',
+    title: 'Finance'
+}, {
+    src: 'growth',
+    id: 'content-growth',
+    title: 'Personal Growth'
+}, {
+    src: 'interview',
+    id: 'content-interview',
+    title: 'Interview Preparation'
+}, {
+    src: 'treasure',
+    id: 'content-treasure',
+    title: `Learner's Treasure`,
+}];
+
+function expandContent(element, id) {
     var content = document.getElementById(id);
     content.classList.toggle("reveal-content");
 }
 
-window.onload = function() {
+function populateContent() {
+    var mainContainer = document.getElementById('posts-section');
+
+    CONTENTS.forEach(content => {
+        var sectionContainer = document.createElement('section');
+        var articleContainer = document.createElement('article');
+        var headerContainer = document.createElement('header');
+        var divContainer = document.createElement('div');
+
+
+        divContainer.className = "content"
+        divContainer.id = content.id;
+        divContainer.innerHTML =
+            `<zero-md src="content/${content.src}/index.md">
+                <template>
+                    <style>
+                        hr {
+                            height: 0.05rem;
+                            margin: 24px 0;
+                            background: #FFF;
+                        }
+                        a {
+                            text-decoration: none;
+                            color: #FFF;
+                        }
+                        a:hover {
+                            color: #4283C4;
+                        }
+                    </style>
+                </template>
+            </zero-md>`;
+
+        headerContainer.innerHTML = content.title;
+        headerContainer.className = "title";
+        headerContainer.addEventListener('click', () => expandContent(headerContainer, content.id));
+
+        articleContainer.className = "accordion";
+
+        sectionContainer.className = "margin-bottom-1";
+
+        articleContainer.appendChild(headerContainer);
+        articleContainer.appendChild(divContainer);
+        sectionContainer.appendChild(articleContainer);
+        mainContainer.appendChild(sectionContainer);
+    });
+}
+
+window.onload = function () {
+
+    this.populateContent();
 
     var visitorsCount = 100;
     var countElement = document.getElementById('visitors');
     var initialCount = 0;
 
-
-    var counter = setInterval(function() {
-        if(initialCount < visitorsCount) {
-            initialCount +=1;
+    var counter = setInterval(function () {
+        if (initialCount < visitorsCount) {
+            initialCount += 1;
             countElement.innerHTML = initialCount;
         } else {
             clearInterval(counter);
